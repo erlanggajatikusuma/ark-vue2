cf54758a6777e3c73b9ca29aae3226c82b91b4cb
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-08-28 18:57:59 +0700

    styling login page

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,23 +1,17 @@
 <template>
-    <div class="container">
-        <h1>Login Page</h1>
-        <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" v-model="password">
-          </div>
-          <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-          </div>
-          <button type="submit" class="btn btn-primary" @click="handleLogin">Submit</button>
-        </form>
-    </div>
+        <div class="login-container d-flex align-items-center justify-content-center">
+          <form class="login-form text-center">
+            <h1 class="font-weight-light text-uppercase mb-5">Login</h1>
+            <div class="form-group">
+              <input type="email" placeholder="email" class="form-control rounded-pill form-control-lg" v-model="email">
+            </div>
+            <div class="form-group">
+              <input type="password" placeholder="password" class="form-control rounded-pill form-control-lg" v-model="password">
+            </div>
+            <button type="submit" class="btn btn-primary btn-block rounded-pill btn-lg mt-5" @click="handleLogin">LOGIN</button>
+            <p class="mt-3 font-weight-normal">Not registered? <router-link to="/register"><strong>Register Now</strong></router-link></p>
+          </form>
+        </div>
 </template>
 
 <script>
@@ -48,5 +42,32 @@ export default {
 </script>
 
 <style scoped>
+  .login-container {
+    width: 100%;
+    height: 100vh;
 
+    font-family: sans-serif;
+    font-size: 14px;
+    background: #eee;
+    color: #666;
+  }
+  .login-form {
+    max-width: 100%;
+    width: 370px;
+    padding: 15px;
+    margin: auto;
+  }
+  .login-form a{
+    text-decoration: none;
+    color: #666;
+  }
+  .form-control {
+    font-size: 15px;
+    min-height: 48px;
+    font-weight: 500;
+  }
+  .form-control:focus{
+    border-color: #723dbe;
+    box-shadow: 0 0 0 0.2rem rgba(114, 61, 190, .25);
+  }
 </style>
