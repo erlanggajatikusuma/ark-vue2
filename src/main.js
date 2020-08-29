934a42ae31328602fa8b214e2f7114f6017fb0ee
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-08-30 02:31:04 +0700

    add vuelidate form register

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuelidate from 'vuelidate'
 import jQuery from 'jquery'
 import 'popper.js'
 import 'bootstrap'
 import './assets/app.scss'
 import { store } from './store/index'
 window.$ = window.jQuery = jQuery
-
+Vue.use(Vuelidate)
 Vue.config.productionTip = false
 
 new Vue({
