77d87ec72834d08c03422aa59b1837becbef6bb8
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-10-06 12:37:52 +0700

    reduce income and orders

--- src/components/history/income-card.vue
+++ src/components/history/income-card.vue
@@ -5,7 +5,7 @@
                 <div class="card">
                     <div class="card-body">
                         <h4>Today's Income</h4>
-                        <h2>Rp. 1.000.000</h2>
+                        <h2>Rp. {{setDay}}</h2>
                         <h4>+2% Yesterday</h4>
                     </div>
                 </div>
@@ -13,8 +13,8 @@
             <div class="col-lg-4 col-md-6 col-sm-12 my-3">
                 <div class="card">
                     <div class="card-body">
-                        <h4>Today's Income</h4>
-                        <h2>Rp. 1.000.000</h2>
+                        <h4>Orders</h4>
+                        <h2>{{setOrders}}</h2>
                         <h4>+2% Yesterday</h4>
                     </div>
                 </div>
@@ -22,8 +22,8 @@
             <div class="col-lg-4 col-md-6 col-sm-12 my-3">
                 <div class="card">
                     <div class="card-body">
-                        <h4>Today's Income</h4>
-                        <h2>Rp. 1.000.000</h2>
+                        <h4>This Year's Income</h4>
+                        <h2>Rp. {{setYear}}</h2>
                         <h4>+2% Yesterday</h4>
                     </div>
                 </div>
@@ -33,8 +33,71 @@
 </template>
 
 <script>
+import axios from 'axios'
+import moment from 'moment'
 export default {
-  name: 'incomeCard'
+  name: 'incomeCard',
+  data () {
+    return {
+      arrToday: [],
+      // arrMonth: [],
+      arrYear: [],
+      orders: []
+    }
+  },
+  computed: {
+    setDay () {
+      if (this.arrToday.length === 0) {
+        return 0
+      } else {
+        const total = this.arrToday.reduce((acc, curr) => acc + curr, 0)
+        return total
+      }
+    },
+    setOrders () {
+      if (this.orders.length === 0) {
+        return 0
+      } else {
+        const total = this.orders.reduce((acc, currentValue) => acc + currentValue, 0)
+        return total
+      }
+    },
+    setYear () {
+      if (this.arrYear.length === 0) {
+        return 0
+      } else {
+        const total = this.arrYear.reduce((acc, currentValue) => acc + currentValue, 0)
+        return total
+      }
+    }
+  },
+  async created () {
+    const { data } = await axios.get(`${process.env.VUE_APP_BASE_URL}/api/v1/history?limit=99999`)
+    data.result.forEach(d => {
+      const date = d.date
+      const today = moment(new Date()).format('D MMMM YYYY')
+
+      // const month = moment(date, 'MMMM').format('MMMM')
+      // const thisMonth = moment().format('MMMM')
+
+      const year = moment(date, 'MMMM').format('YYYY')
+      const thisYear = moment().format('YYYY')
+
+      const { amount } = d
+      if (date === today) {
+        this.arrToday.push(amount)
+      }
+      // if (month === thisMonth) {
+      //   this.arrMonth.push(amount)
+      // }
+      if (year === thisYear) {
+        this.arrYear.push(amount)
+      }
+
+      const order = d.orders.split(',')
+      this.orders.push(order.length)
+    })
+  }
 }
 </script>
 
